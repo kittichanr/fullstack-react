51aca05a2db13c1b8f47994db832c4cfca622a83
petchkubbb <>
2021-04-25 07:47:41 +0700

    finish authentication flow

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// dont commit this
+module.exports = {
+    googleCientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+}
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+const passport = require('passport');
+require('./models/user')
 require('./services/passport')
 
+mongoose.connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
 const app = express()
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 
