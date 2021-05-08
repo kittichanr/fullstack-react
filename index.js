e6dd21b0c2939f1d2c0b18c74a42893c40da3d61
petchkubbb <>
2021-05-08 08:42:03 +0700

    setup route in prod

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.connect(keys.mongoURI, {
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json());   
+app.use(express.json());
 
 app.use(cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -26,6 +26,15 @@ app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
