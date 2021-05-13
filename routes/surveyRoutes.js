2924b19eda35222725c2a1d16ed3c0dd3487da5d
petchkubbb <>
2021-05-13 21:27:34 +0700

    add sengrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const requireLogin = require('../middlewares/requireLogin.js')
 const requireCredits = require('../middlewares/requireCredits')
+const sgMail = require("@sendgrid/mail");
 
 const Survey = mongoose.model('surveys')
 
@@ -19,5 +20,16 @@ module.exports = app => {
                 _user: req.user.id,
                 dateSent: Date.now()
             })
+
+            sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+            
+            const msg = {
+                to: "test@example.com",
+                from: "test@example.com",
+                subject: "Sending with Twilio SendGrid is Fun",
+                text: "and easy to do anywhere, even with Node.js",
+                html: `<div> ${survey.body} </div>`,
+            };
+            sgMail.send(msg);
         })
 }
